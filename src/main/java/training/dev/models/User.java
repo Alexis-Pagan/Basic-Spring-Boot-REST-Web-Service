1102300830d53941b09ee3a4f0a812204735d3aa
Alexis Garcia <alexisgarciapagan@gmail.com>
2019-01-10 23:14:40 -0400

    Add files via upload

---
+++ src/main/java/training/dev/models/User.java
@@ -0,0 +1,14 @@
+package training.dev.models;
+
+public class User {
+	
+	private String email;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
